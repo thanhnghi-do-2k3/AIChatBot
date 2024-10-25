9896435a137f57bd24a4ae4390a88efd585fc09a
thanhnghi-do-2k3 <nhokcrboy@gmail.com>
2024-10-25 12:49:25 +0700

    fix barbel resolve

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
       'module-resolver',
       {
         root: ['./src'],
+        extensions: ['.js', '.json', '.d.ts', '.tsx', '.ts'],
         alias: {
           hooks: './src/hooks',
           components: './src/components',
           theme: './src/theme',
           navigation: './src/navigation',
           screens: './src/screens',
+          constant: './src/constants',
         },
       },
+      'nativewind/babel',
     ],
   ],
 };
